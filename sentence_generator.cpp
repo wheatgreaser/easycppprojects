a7bc80dfaa06acbbca56fa89ddbe383abfb30d4e
wheatgreaser <wheatgreaser3000@gmail.com>
2024-07-31 19:38:05 +0530

    done done done

--- sentence_generator.cpp
+++ sentence_generator.cpp
@@ -11,12 +11,12 @@ int main(){
     String tokenarray[100];
     std::istringstream iss(sentence); //input stream
     String token;
-    int a_a = 0, a_b = 0, a_c = 0, a_d = 0, b_a = 0, b_b = 0, b_c = 0, b_d = 0, c_a = 0, c_b = 0, c_c = 0, c_d = 0, d_a = 0, d_b = 0, d_c = 0, d_d = 0;
+    float a_a = 0, a_b = 0, a_c = 0, a_d = 0, b_a = 0, b_b = 0, b_c = 0, b_d = 0, c_a = 0, c_b = 0, c_c = 0, c_d = 0, d_a = 0, d_b = 0, d_c = 0, d_d = 0;
     
-    int p_apple[10];
-    int p_ball[10];
-    int p_cat[10];
-    int p_dog[10];
+    float p_apple[4];
+    float p_ball[4];
+    float p_cat[4];
+    float p_dog[4];
 
     int j = 0;
     while(std::getline(iss, token, ' ')){ //getline takes string from the input stream
@@ -78,11 +78,75 @@ int main(){
             }
     }
     }
+
+    // computing the probabilities
+    p_apple[0] = a_a / (a_a + a_b + a_c + a_d);
+    p_apple[1] = a_b / (a_a + a_b + a_c + a_d);
+    p_apple[2] = a_c / (a_a + a_b + a_c + a_d);
+    p_apple[3] = a_d / (a_a + a_b + a_c + a_d);
+
+    p_ball[0] = b_a / (b_a + b_b + b_c + b_d);
+    p_ball[1] = b_b / (b_a + b_b + b_c + b_d);
+    p_ball[2] = b_c / (b_a + b_b + b_c + b_d);
+    p_ball[3] = b_d / (b_a + b_b + b_c + b_d);
+
+    p_cat[0] = c_a / (c_a + c_b + c_c + c_d);
+    p_cat[1] = c_b / (c_a + c_b + c_c + c_d);
+    p_cat[2] = c_c / (c_a + c_b + c_c + c_d);
+    p_cat[3] = c_d / (c_a + c_b + c_c + c_d);
+
+    p_dog[0] = d_a / (d_a + d_b + d_c + d_d);
+    p_dog[1] = d_b / (d_a + d_b + d_c + d_d);
+    p_dog[2] = d_c / (d_a + d_b + d_c + d_d);
+    p_dog[3] = d_d / (d_a + d_b + d_c + d_d);
     
-    std::cout << a_a << std::endl; 
-    std::cout << a_b << std::endl;
-    std::cout << a_c << std::endl;
-    std::cout << a_d << std::endl;
+    for(int i = 0; i < 4; i++){
+        std::cout << p_ball[i] <<std::endl;
+    }
+
+    int max1, max2, max3, max4;
+    max1 = p_apple[0];
+    max2 = p_ball[0];
+    max3 = p_cat[0];
+    max4 = p_dog[0];
+    int pred_apple_index, pred_ball_index, pred_cat_index, pred_dog_index;
+
+    for(int i = 0; i < 4; i++){   
+        if(max1 < p_apple[i]){
+            max1 = p_apple[i];
+            pred_apple_index = i;
+            break;
+
+        }
+    }
+
+    for(int i = 0; i < 4; i++){   
+        if(max2 < p_ball[i]){
+            max2 = p_ball[i];
+            pred_ball_index = i;
+            break;
+
+        }
+    }
+
+    for(int i = 0; i < 4; i++){   
+        if(max3 < p_cat[i]){
+            max3 = p_cat[i];
+            pred_cat_index = i;
+            break;
+
+        }
+    }
+
+    for(int i = 0; i < 4; i++){   
+        if(max4 < p_dog[i]){
+            max4 = p_dog[i];
+            pred_dog_index = i;
+            break;
+
+        }
+    }
+
     
     
     return 0;
